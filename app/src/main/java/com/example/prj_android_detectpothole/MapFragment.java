1be44b0dcd17934f1bfa94553c46adc62527a605
minhhieu151004 <tkl4hml48@gmail.com>
2024-11-08 18:32:59 +0700

    enable UserLocation,add btn_GPS

--- app/src/main/java/com/example/prj_android_detectpothole/MapFragment.java
+++ app/src/main/java/com/example/prj_android_detectpothole/MapFragment.java
@@ -2,13 +2,18 @@ package com.example.prj_android_detectpothole;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,6 +37,15 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.prj_android_detectpothole.OBJECT.MyMarker;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -40,43 +54,64 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class MapFragment extends Fragment implements
-        GoogleMap.OnMyLocationButtonClickListener,
-        GoogleMap.OnMyLocationClickListener,
         OnMapReadyCallback,
         ActivityCompat.OnRequestPermissionsResultCallback,
-        GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener, GoogleMap.OnCameraIdleListener{
+        GoogleMap.OnCameraIdleListener {
 
+    final public String TAG = "FragmentMap";
     //Khai báo
     View view;
     LinearLayout linear_select_location;
     Button btn_Show_Pothole, btn_Add_Pothole, btn_goback, btn_ok;
-    ImageButton btn_zoom_in,btn_zoom_out;
+    ImageButton btn_zoom_in, btn_zoom_out, btn_moveToMyLocation;
     EditText edt_Serch;
-    private GoogleMap map;
+
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private boolean permissionDenied = false;
     private boolean isAddPotholeMode = false;
+
+    private GoogleMap map;
     List<Marker> listMyMark;
     Marker mark;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
 
-    void findID(View view){
+    LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(@NonNull LocationResult locationResult) {
+            if(locationResult == null){
+                return;
+            }
+            for (Location location: locationResult.getLocations()){
+                //LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                //map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+        }
+    };
+
+    void findID(View view) {
         btn_Show_Pothole = view.findViewById(R.id.btn_show_pothole);
         btn_Add_Pothole = view.findViewById(R.id.btn_add_pothole);
         btn_goback = view.findViewById(R.id.btn_goBack);
         btn_ok = view.findViewById(R.id.btn_ok);
         btn_zoom_in = view.findViewById(R.id.btn_zoom_in);
         btn_zoom_out = view.findViewById(R.id.btn_zoom_out);
+        btn_moveToMyLocation = view.findViewById(R.id.btn_moveToMyLocation);
         edt_Serch = view.findViewById(R.id.edt_Serch);
         linear_select_location = view.findViewById(R.id.notify_select_location);
         linear_select_location.setVisibility(View.GONE);
         listMyMark = new ArrayList<>();
     }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -84,6 +119,11 @@ public class MapFragment extends Fragment implements
         findID(view);
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(200);
+        locationRequest.setFastestInterval(100);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
         //Event Button click
         btn_Show_Pothole.setOnClickListener(new View.OnClickListener() {
@@ -95,12 +135,7 @@ public class MapFragment extends Fragment implements
         btn_Add_Pothole.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                edt_Serch.setVisibility(View.GONE);
-                btn_Add_Pothole.setVisibility(View.GONE);
-                btn_Show_Pothole.setVisibility(View.GONE);
-                btn_zoom_in.setVisibility(View.GONE);
-                btn_zoom_out.setVisibility(View.GONE);
-                linear_select_location.setVisibility(View.VISIBLE);
+                ViewGONE();
                 isAddPotholeMode = true;
                 LatLng center = map.getCameraPosition().target;
                 mark = map.addMarker(new MarkerOptions().position(center));
@@ -109,12 +144,7 @@ public class MapFragment extends Fragment implements
         btn_goback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                edt_Serch.setVisibility(View.VISIBLE);
-                btn_Add_Pothole.setVisibility(View.VISIBLE);
-                btn_Show_Pothole.setVisibility(View.VISIBLE);
-                btn_zoom_in.setVisibility(View.VISIBLE);
-                btn_zoom_out.setVisibility(View.VISIBLE);
-                linear_select_location.setVisibility(View.GONE);
+                ViewVISIBLE();
                 isAddPotholeMode = false;
                 if (mark != null) {
                     mark.remove();
@@ -129,14 +159,34 @@ public class MapFragment extends Fragment implements
         });
         btn_zoom_in.setOnClickListener(v -> map.animateCamera(CameraUpdateFactory.zoomIn()));
         btn_zoom_out.setOnClickListener(v -> map.animateCamera(CameraUpdateFactory.zoomOut()));
+        btn_moveToMyLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zoomToUserLocation();
+            }
+        });
         return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        StopLocationUpdates();
+    }
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
-        map.setOnMyLocationButtonClickListener(this);
-        map.setOnMyLocationClickListener(this);
         enableMyLocation();
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        map.getUiSettings().setMapToolbarEnabled(false);
+        map.setInfoWindowAdapter(new MyIn4WindowAdapter(getActivity()));
         map.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
             @Override
             public void onCameraMove() {
@@ -155,82 +205,28 @@ public class MapFragment extends Fragment implements
                 return false;
             }
         });
-        map.setOnMapClickListener(this::onMapClick);
-        map.setOnMapLongClickListener(this::onMapLongClick);
         map.setOnCameraIdleListener(this::onCameraIdle);
-        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        map.setMyLocationEnabled(false);
-
-        MyMarker myMarker = new MyMarker(1,10.869633,106.803269,"8 Street, Linh Xuan, Thu Duc, HCM City","HIGH");
-
-        MarkerOptions markerOpt = new MarkerOptions();
-        LatLng test = new LatLng(10.869633, 106.803269);
-        markerOpt.position(test)
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.error));
-        map.addMarker(markerOpt).setTag(myMarker);
-
-        myMarker = new MyMarker(1,10.873079,106.807883,"8 Street, Linh Xuan, Thu Duc, HCM City","MEDIUM");
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(10.873079,106.807883))
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.warning))).setTag(myMarker);
-
-        myMarker = new MyMarker(1,10.864635,106.799511,"8 Street, Linh Xuan, Thu Duc, HCM City","LOW");
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(10.864635,106.799511))
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.info))).setTag(myMarker);
-
-        MyIn4WindowAdapter adapter = new MyIn4WindowAdapter(getActivity());
-        map.setInfoWindowAdapter(adapter);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(test,15.0f));
 
     }
 
     @Override
     public void onCameraIdle() {
-        if(isAddPotholeMode){
+        if (isAddPotholeMode) {
             LatLng center = map.getCameraPosition().target;
             mark = map.addMarker(new MarkerOptions().position(center));
         }
     }
 
     @Override
-    public void onMapClick(@NonNull LatLng latLng) {
-        //String addr = getAddressFromLatLng(latLng.latitude,latLng.longitude);
-        //Toast.makeText(getActivity(), "Addr: " + addr, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onMapLongClick(@NonNull LatLng latLng) {
-
-    }
-
-    @Override
-    public boolean onMyLocationButtonClick() {
-        return false;
-    }
-
-    @Override
-    public void onMyLocationClick(@NonNull Location location) {
-
-    }
-    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 enableMyLocation();
-            }
-            else {
+                zoomToUserLocation();
+                CheckSettingAndStartLocationUpdates();
+            } else {
                 Toast.makeText(getActivity(), "Quyền vị trí bị từ chối", Toast.LENGTH_SHORT).show();
             }
 
@@ -238,6 +234,78 @@ public class MapFragment extends Fragment implements
         }
     }
 
+    private void zoomToUserLocation() {
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+        locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18));
+            }
+        });
+        locationTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+    private void CheckSettingAndStartLocationUpdates() {
+        LocationSettingsRequest locationSettingsRequest = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest).build();
+        SettingsClient client = LocationServices.getSettingsClient(getActivity());
+
+        Task<LocationSettingsResponse> locationSettingsResponseTask = client.checkLocationSettings(locationSettingsRequest);
+        locationSettingsResponseTask.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                StartLocationUpdates();
+            }
+        });
+        locationSettingsResponseTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    ResolvableApiException apiException = (ResolvableApiException) e;
+                    try {
+                        apiException.startResolutionForResult(getActivity(), 1001);
+                    } catch (IntentSender.SendIntentException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void StartLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+    }
+    private void StopLocationUpdates(){
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+            zoomToUserLocation();
+            CheckSettingAndStartLocationUpdates();
+            return;
+        }
+        ActivityCompat.requestPermissions(getActivity(),new String[] {
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION},LOCATION_PERMISSION_REQUEST_CODE);
+    }
     private void openDialogShowPothole() {
         final Dialog dialog = new Dialog(getContext());
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -258,7 +326,6 @@ public class MapFragment extends Fragment implements
 
         dialog.show();
     }
-
     private void openDialogAddPothole() {
         final Dialog dialog = new Dialog(getContext());
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -350,12 +417,7 @@ public class MapFragment extends Fragment implements
                 }
                 Toast.makeText(getActivity(), "Submit successfully!", Toast.LENGTH_SHORT).show();
                 dialog.dismiss();
-                edt_Serch.setVisibility(View.VISIBLE);
-                btn_Add_Pothole.setVisibility(View.VISIBLE);
-                btn_Show_Pothole.setVisibility(View.VISIBLE);
-                btn_zoom_out.setVisibility(View.VISIBLE);
-                btn_zoom_in.setVisibility(View.VISIBLE);
-                linear_select_location.setVisibility(View.GONE);
+                ViewVISIBLE();
                 isAddPotholeMode = false;
             }
         });
@@ -379,18 +441,22 @@ public class MapFragment extends Fragment implements
             return "NULL";
         }
     }
-
-    @SuppressLint("MissingPermission")
-    private void enableMyLocation() {
-        if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED
-                || ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            map.setMyLocationEnabled(true);
-            return;
-        }
-        ActivityCompat.requestPermissions(getActivity(),new String[] {
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION},LOCATION_PERMISSION_REQUEST_CODE);
+    private void ViewVISIBLE(){
+        edt_Serch.setVisibility(View.VISIBLE);
+        btn_Add_Pothole.setVisibility(View.VISIBLE);
+        btn_Show_Pothole.setVisibility(View.VISIBLE);
+        btn_zoom_in.setVisibility(View.VISIBLE);
+        btn_zoom_out.setVisibility(View.VISIBLE);
+        btn_moveToMyLocation.setVisibility(View.VISIBLE);
+        linear_select_location.setVisibility(View.GONE);
+    }
+    private void ViewGONE(){
+        edt_Serch.setVisibility(View.GONE);
+        btn_Add_Pothole.setVisibility(View.GONE);
+        btn_Show_Pothole.setVisibility(View.GONE);
+        btn_zoom_in.setVisibility(View.GONE);
+        btn_zoom_out.setVisibility(View.GONE);
+        btn_moveToMyLocation.setVisibility(View.GONE);
+        linear_select_location.setVisibility(View.VISIBLE);
     }
 }
